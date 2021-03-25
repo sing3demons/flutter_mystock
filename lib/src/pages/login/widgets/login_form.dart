171b802912d5f54f44505469474ea3be98e07074
kumneung <sing3demons@live.com>
2021-03-25 07:01:44 +0700

    login_page ->form input add buttom login

--- lib/src/pages/login/widgets/login_form.dart
+++ lib/src/pages/login/widgets/login_form.dart
@@ -1,13 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mystock/src/config/theme.dart' as cuttom_theme;
 
 class LoginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: FormInput(),
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        _buildForm(),
+        _buildSubmitFormButton(),
+      ],
+    );
+  }
+
+  Card _buildForm() => Card(
+        margin: EdgeInsets.only(bottom: 22, left: 22, right: 22),
+        elevation: 2.0,
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(22.0),
+        ),
+        child: Padding(
+          padding:
+              const EdgeInsets.only(top: 20, bottom: 58, left: 28, right: 28),
+          child: FormInput(),
+        ),
+      );
+
+  Container _buildSubmitFormButton() => Container(
+        width: 220,
+        height: 50,
+        decoration: _boxDecoration(),
+        child: FlatButton(
+          onPressed: () {
+            //
+          },
+          child: Text(
+            'LOGIN',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25.0,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+      );
+
+  BoxDecoration _boxDecoration() {
+    final gradientStart = cuttom_theme.Theme.gradientStart;
+    final gradientEnd = cuttom_theme.Theme.gradientEnd;
+
+    final boxShadowItem = (Color color) => BoxShadow(
+          color: color,
+          offset: Offset(1.0, 6.0),
+          blurRadius: 20.0,
+        );
+
+    return BoxDecoration(
+      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+      boxShadow: [
+        boxShadowItem(gradientStart),
+        boxShadowItem(gradientEnd),
+      ],
+      gradient: LinearGradient(
+        colors: [
+          gradientEnd,
+          gradientStart,
+        ],
+        begin: const FractionalOffset(0.0, 0.0),
+        end: const FractionalOffset(1.0, 1.0),
+        stops: [0.0, 1.0],
       ),
     );
   }
